0c5dd3c926eab900c480ed763f5407d68b1bdbb7
Matt Wozniski <godlygeek+git@gmail.com>
2016-08-08 22:58:24 -0400

    Fix an incorrect comment in constants.py

--- constants.py
+++ constants.py
@@ -1,7 +1,7 @@
 # 20 lights down, then 20 lights back up at the same heights, 5 times.
 _rows = (range(20)[::-1] + range(20)[::]) * 5
 
-# 20 lights switching between cols 1 and 2, then 20 switching between 4 and 3,
+# 20 lights switching between cols 0 and 1, then 20 switching between 3 and 2,
 # repeated 5 times offset by 4 columns per repetition.
 _cols = [v + 4 * i for i in range(5)
          for v in [j % 2 for j in range(20)] + [3 - j % 2 for j in range(20)]
